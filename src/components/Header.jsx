8e8a0bd7f8b5ef20c85e891073d1488827b47f99
wojtko <j.w2001@onet.pl>
2023-08-06 11:55:18 +0200

    Create right menu. Add react-reveal and material-ui module.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import MenuIcon from "@material-ui/icons/Menu";
 
 function Header() {
   return (
@@ -9,17 +10,19 @@ function Header() {
       </a>
 
       <Menu>
-        <p><a href="#">Vehicles</a></p>
-        <p><a href="#">Energy</a></p>
-        <p><a href="#">Charging</a></p>
-        <p><a href="#">Discover</a></p>
-        <p><a href="#">Shop</a></p>
+        <a href="#">Vehicles</a>
+        <a href="#">Energy</a>
+        <a href="#">Charging</a>
+        <a href="#">Discover</a>
+        <a href="#">Shop</a>
       </Menu>
 
       <RightMenu>
+        <a href="#">Shop</a>
+        <a href="#">Tesla Account</a>
 
+        <CustomMenu />
       </RightMenu>
-
     </Container>
   );
 }
@@ -27,29 +30,49 @@ function Header() {
 export default Header;
 
 const Container = styled.div`
-    top: 0;
-    left: 0;
-    right: 0;
-    min-height: 60px;
-    position: fixed;
-    display: flex;
-    align-items: center;
-    padding: 0 40px;
+  top: 0;
+  left: 0;
+  right: 0;
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 40px;
 `;
 
 const Menu = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
 
-    p {
-        font-weight: 600;
-        text-transform: uppercase;
-        padding: 0 10px;
-        flex-wrap: nowrap;
-        font-size: 14px;
-    }
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+    font-size: 14px;
+  }
+
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
-const RightMenu = styled.div``;
+const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+    font-size: 14px;
+  }
+`;
+
+const CustomMenu = styled(MenuIcon)`
+  cursor: pointer;
+`;
