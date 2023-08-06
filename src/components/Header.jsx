83bd8002b1fd2e7445be959756837328214d3267
wojtko <j.w2001@onet.pl>
2023-08-06 13:04:11 +0200

    Create burger menu. Add click event listeners.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
 
 function Header() {
+  const [burgerStatus, setBurgerStatus] = useState(0);
+
   return (
     <Container>
       <a href="#">
@@ -20,9 +23,33 @@ function Header() {
       <RightMenu>
         <a href="#">Shop</a>
         <a href="#">Tesla Account</a>
-
-        <CustomMenu />
+        <CustomMenu onClick={() => setBurgerStatus(1)} />
       </RightMenu>
+
+      <BurgerNav show={burgerStatus}>
+        <CloseWrapper>
+          <CustomCloseIcon onClick={() => setBurgerStatus(0)} />
+        </CloseWrapper>
+
+        <li>
+          <a href="#">Vehicles</a>
+        </li>
+        <li>
+          <a href="#">Energy</a>
+        </li>
+        <li>
+          <a href="#">Charging</a>
+        </li>
+        <li>
+          <a href="#">Discover</a>
+        </li>
+        <li>
+          <a href="#">Shop</a>
+        </li>
+        <li>
+          <a href="#">Support</a>
+        </li>
+      </BurgerNav>
     </Container>
   );
 }
@@ -35,10 +62,11 @@ const Container = styled.div`
   right: 0;
   min-height: 60px;
   position: fixed;
+  z-index: 1;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 0 40px;
+  padding: 0 20px;
 `;
 
 const Menu = styled.div`
@@ -75,4 +103,38 @@ const RightMenu = styled.div`
 
 const CustomMenu = styled(MenuIcon)`
   cursor: pointer;
+`;
+
+const BurgerNav = styled.div`
+  position: fixed;
+  z-index: 100;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background: #fff;
+  width: 300px;
+  list-style: none;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  transform: ${(props) => (props.show ? "translateX(0)" : "translateX(100%)")};
+
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+
+    a {
+      font-weight: 600;
+    }
+  }
+`;
+
+const CustomCloseIcon = styled(CloseIcon)`
+  cursor: pointer;
+`;
+
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
 `;
